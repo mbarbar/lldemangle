85b8d4186b7581fb320ccd880be52c9825e4e818
Mohamad Barbar <mbarbar@runbox.com>
2018-10-19 17:43:19 +1100

    Read mangled names from BC files.

--- bcdemangle.py
+++ bcdemangle.py
@@ -13,10 +13,29 @@ options:
   [todo] todo
 """.format(sys.argv[0])
 
+# Should work with clang.
+MANGLED_NAME_RE = re.compile("_Z[a-zA-Z0-9_]*")
+
+def read_mangled_names(bcfile):
+    mangled_names = set()
+
+    with open(bcfile, "r") as f:
+        for line in f.readlines():
+            line_mangled_names = re.findall(MANGLED_NAME_RE, line)
+            for mangled_name in line_mangled_names:
+                mangled_names.add(mangled_name)
+
+    return mangled_names
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print USAGE
         sys.exit(1)
 
     bcfile = sys.argv[1]
+
+    mangled_names = read_mangled_names(bcfile)
+    for i in mangled_names:
+        print i
 
